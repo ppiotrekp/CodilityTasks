d080800a51cb4b7bd060c0e29295db74f7d8c971
Piotr Pyrczak <ppyrczak5@gmail.com>
2021-08-30 22:28:57 +0200

    Update CountFactorsV2.java

--- CountFactorsV2.java
+++ CountFactorsV2.java
@@ -1,3 +1,9 @@
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
 class Solution {
     public int solution(int N) {
         int i = 1;
